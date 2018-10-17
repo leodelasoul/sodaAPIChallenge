58bb9f81d3a08d6665d69c3a42e614f52b375e3c
unknown <l.ricker93@web.de>
2018-10-17 20:32:50 +0200

    added search functionality

--- js/main.js
+++ js/main.js
@@ -3,16 +3,60 @@ const titleUl = document.getElementById('titleUl');
 const releaseUl = document.getElementById('releaseUl');
 
 
-var count = 0;
-
 function recieveJSON() {
   var request = new XMLHttpRequest();
-  request.open('GET', "https://data.sfgov.org/resource/wwmu-gmzc.json?$select=director,release_year,title", true)
+  request.open('GET', "https://data.sfgov.org/resource/wwmu-gmzc.json?$select=director,release_year,title&$limit=10", true)
   request.onload = function(){
     if(request.status >= 200 && request.status < 400){
-      var index = [];
       var rawJson = JSON.parse(this.response);
+          JSONtoList(rawJson, removeList);
+    }
 
+  else{
+    console.log("error");
+  }
+}
+  request.send();
+
+}
+
+
+function searchFor(){
+  var searchString = document.getElementById("searchField").value;
+  searchString = "'".concat(searchString,"'")
+  var request = new XMLHttpRequest();
+
+  //var test = "https://data.sfgov.org/resource/wwmu-gmzc.json?$select=director,release_year,title&$where=title="+searchString;
+  request.open('GET', "https://data.sfgov.org/resource/wwmu-gmzc.json?$select=director,release_year,title&$where=title="+searchString, true);
+  request.onload = function(){
+    if(request.status >= 200 && request.status < 400){
+      var rawJson = JSON.parse(this.response);
+      console.log(rawJson)
+      JSONtoList(rawJson,removeList);
+
+  }
+  else{
+    console.log("error");
+  }
+  }
+  request.send()
+}
+
+
+function removeList(rawJson){
+  //console.log(rawJson.);
+    while (directorUl.firstChild) {
+      directorUl.removeChild(directorUl.firstChild);
+      titleUl.removeChild(titleUl.firstChild);
+      releaseUl.removeChild(releaseUl.firstChild);
+    }
+}
+
+function JSONtoList(rawJson, removeList){
+    if(Boolean(titleUl.childElementCount > 0) && Boolean(directorUl.childElementCount > 0) && Boolean(releaseUl.childElementCount > 0)){
+      return removeList(rawJson);
+    }
+    else {
       rawJson.forEach(filmLoc =>{
         li = document.createElement('li');
         //li.textContent = JSON.stringify(filmLoc)
@@ -28,11 +72,7 @@ function recieveJSON() {
         releaseUl.appendChild(li2);
 
       });
-
-}
-    else{
-      console.log("error");
     }
-}
-  request.send();
+
+
 }
